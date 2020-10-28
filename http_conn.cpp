48e85168cb03882e36d87db153485cd344a710cb
DENGZEYI <271098327@qq.com>
2020-10-28 11:49:05 +0800

    add vscode config file

--- http_conn.cpp
+++ http_conn.cpp
@@ -11,7 +11,7 @@ const char* error_404_form = "The requested file was not found on this server.\n
 const char* error_500_title = "Internal Error";
 const char* error_500_form = "There was an unusual problem serving the requested file.\n";
 //网站的根目录
-const char* doc_root = "/var/www/html";
+const char* doc_root = "/home/dengzeyi/MyHttpServer/www/html";
 
 int setnonblocking( int fd )
 {
@@ -334,7 +334,7 @@ http_conn::HTTP_CODE http_conn::do_request()
     strcpy( m_real_file, doc_root );
     int len = strlen( doc_root );
     strncpy( m_real_file + len, m_url, FILENAME_LEN - len - 1 );
-    if ( stat( m_real_file, &m_file_stat ) < 0 )
+    if ( int ret=stat( m_real_file, &m_file_stat ) < 0 )
     {
         return NO_RESOURCE;
     }
